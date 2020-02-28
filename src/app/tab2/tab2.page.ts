97e7e360be44edf1d3a3b773c347021bc14c2668
tothattila <tthttl@gmail.com>
2020-02-28 16:20:04 +0100

    SWE-124971: refactoring android tab

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 
@@ -13,7 +13,8 @@ const constraints = {
             max: 1440
         },
         facingMode: 'environment'
-    }
+    },
+    audio: false
 };
 
 @Component({
@@ -21,24 +22,26 @@ const constraints = {
     templateUrl: 'tab2.page.html',
     styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page{
+export class Tab2Page implements AfterViewInit {
 
-    @ViewChild('videoContainer', {static: true}) videoContainer;
-    private video;
-    private stream;
+    @ViewChild('video', {static: true}) video;
+    @ViewChild('canvas', {static: true}) canvas;
+    @ViewChild('photo', {static: true}) photo;
+    width = 320;
+    height = 0;
+    streaming = false;
+    stream: MediaStream;
 
     constructor(private platform: Platform, private androidPermissions: AndroidPermissions) {
-        this.video = document.createElement('video');
-        this.video.width = 320;
-        this.video.height = 240;
-        this.video.setAttribute('autoplay', '');
     }
 
-    ngAfterViewInit() {
-        this.videoContainer.nativeElement.appendChild(this.video);
+    ngAfterViewInit(): void {
+        this.video.nativeElement.width = 320;
+        this.video.nativeElement.height = 240;
+        this.video.nativeElement.setAttribute('autoplay', '');
     }
 
-    ionViewDidEnter(){
+    ionViewDidEnter() {
         if (this.platform.is('android')) {
             this.platform.ready().then(async () => {
                 try {
@@ -46,6 +49,7 @@ export class Tab2Page{
                     if (!permissionResponse.hasPermission) {
                         await this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA]);
                     }
+                    this.startup();
                 } catch (error) {
                     console.log(error);
                 }
@@ -53,30 +57,58 @@ export class Tab2Page{
         }
     }
 
-    play(){
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(stream => this.handleSuccess(stream))
-            .catch(error => this.handleError(error));
+    setSize() {
+        if (!this.streaming) {
+            this.height = this.video.videoHeight / (this.video.videoWidth / this.width);
+            if (isNaN(this.height)) {
+                this.height = this.width / (4 / 3);
+            }
+            this.video.nativeElement.setAttribute('width', this.width);
+            this.video.nativeElement.setAttribute('height', this.height);
+            this.canvas.nativeElement.setAttribute('width', this.width);
+            this.canvas.nativeElement.setAttribute('height', this.height);
+            this.streaming = true;
+        }
     }
 
-    handleSuccess (stream: MediaStream) {
+    takepicture() {
+        const context = this.canvas.nativeElement.getContext('2d');
+        if (this.width && this.height) {
+            this.canvas.nativeElement.width = this.width;
+            this.canvas.nativeElement.height = this.height;
+            context.drawImage(this.video.nativeElement, 0, 0, this.width, this.height);
+            const data = this.canvas.nativeElement.toDataURL('image/png');
+            this.photo.nativeElement.setAttribute('src', data);
+        } else {
+            this.clearphoto();
+        }
+    }
+
+    ionViewDidLeave() {
+        if (this.video.nativeElement && this.video.nativeElement.srcObject) {
+            this.video.nativeElement.srcObject.getTracks().forEach(track => track.stop());
+        }
+    }
+
+    private async startup() {
         if (this.stream) {
             this.stream.getTracks().forEach(track => track.stop());
         }
-        this.stream = stream;
-        this.video.srcObject = this.stream;
-    };
-
-    handleError (error: any) {
-        console.log(error);
-        const p = document.createElement('p');
-        p.innerHTML = 'navigator.getUserMedia error: ' + error.name + ', ' + error.message;
-        this.videoContainer.nativeElement.appendChild(p);
-    };
-
-    ionViewDidLeave() {
-        if (this.video && this.video.srcObject) {
-            this.video.srcObject.getTracks().forEach(track => track.stop());
+        try {
+            this.stream = await navigator.mediaDevices.getUserMedia(constraints);
+            this.video.nativeElement.srcObject = this.stream;
+            this.video.nativeElement.play();
+        } catch (err) {
+            console.log("An error occurred: " + err);
         }
+        this.clearphoto();
+    }
+
+    private clearphoto() {
+        const context = this.canvas.nativeElement.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+        const data = this.canvas.nativeElement.toDataURL('image/png');
+        this.photo.nativeElement.setAttribute('src', data);
     }
 }
